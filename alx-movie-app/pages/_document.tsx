6184d945a9de5c1e3a3a4b7c427c6eacafae7e2e
amosamue <samuelamoh130@gmail.com>
2025-10-20 06:30:18 +0000

    Task 3: Link PWA manifest in _document.tsx

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Set theme color for mobile browser */}
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
